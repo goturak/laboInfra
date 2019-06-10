4b697cf12bb79645d75f2b123e92c948ccf7fb4b
Goturak <luca-manu.reisdecarvalho@gmail.com>
2019-06-10 18:38:19 +0200

    finished step 2

--- express-image/src/index.js
+++ express-image/src/index.js
@@ -1,4 +1,46 @@
 var Chance = require ('chance');
 var chance= new Chance();
 
-console.log("Bonjour "+ chance.name());
+var express= require('express');
+var app= express(); 
+
+
+app.get('/test', function (req, res) {
+  res.send('Hello World! this is a test')
+})
+app.get('/', function (req, res) {
+  res.send(generateStudent())
+})
+
+app.listen(3000,function(){
+	console.log("accepting http req on port 3000")
+})
+
+function generateStudent(){
+	var numberOfStudents= chance.integer({
+		min:0,
+		max:10
+	});
+	console.log(numberOfStudents);
+	var students=[];
+
+	for(var i=0; i< numberOfStudents;i++){
+		var gender= chance.gender();
+		var birthYear= chance.year({
+			min:1986,
+			max:1996});
+
+		students.push({
+			firstName:chance.first({
+				gender:gender
+			}),
+			lastName :chance.last(),
+			gender:gender,
+			birthday:chance.birthday({
+				year:birthYear
+			})
+		});
+	};
+	console.log(students);
+	return students;
+}
